1d58cd872c51785891c91941ef2b3f47f73a9a09
Michal Moskal <michal@moskal.me>
2020-09-16 17:43:26 +0200

    Use new SensorAggregatorHost

--- tfjacdac.ts
+++ tfjacdac.ts
@@ -1,17 +1,8 @@
 namespace jd_class {
-    export const TFLITE = 0x13fe118c
+    export const TFLITE = 0x140f9a78
 }
 
 namespace jacdac {
-    export enum TFLiteSampleType { // uint8_t
-        U8 = 0x8,
-        I8 = 0x88,
-        U16 = 0x10,
-        I16 = 0x90,
-        U32 = 0x20,
-        I32 = 0xa0,
-    }
-
     export enum TFLiteCmd {
         /**
          * Argument: model_size bytes uint32_t. Open pipe for streaming in the model. The size of the model has to be declared upfront.
@@ -29,18 +20,12 @@ namespace jacdac {
 
     export enum TFLiteReg {
         /**
-         * Set automatic input collection.
-         * These settings are stored in flash.
-         */
-        Inputs = 0x80,
-
-        /**
          * Read-write uint16_t. When register contains `N > 0`, run the model automatically every time new `N` samples are collected.
          * Model may be run less often if it takes longer to run than `N * sampling_interval`.
          * The `outputs` register will stream its value after each run.
          * This register is not stored in flash.
          */
-        AutoInvokeEvery = 0x81,
+        AutoInvokeEvery = 0x80,
 
         /** Read-only bytes. Results of last model invocation as `float32` array. */
         Outputs = 0x101,
@@ -60,20 +45,8 @@ namespace jacdac {
         /** Read-only bytes uint32_t. The size of `.tflite` model in bytes. */
         ModelSize = 0x184,
 
-        /** Read-only uint32_t. Number of input samples collected so far. */
-        NumSamples = 0x185,
-
-        /** Read-only bytes uint8_t. Size of a single sample. */
-        SampleSize = 0x186,
-
-        /** Read-write uint8_t. When set to `N`, will stream `N` samples as `current_sample` reading. */
-        StreamSamples = 0x82,
-
-        /** Read-only bytes. Last collected sample. */
-        CurrentSample = 0x187,
-
-        /** Read-only string (bytes). Textual description of last error when running model (if any). */
-        LastError = 0x188,
+        /** Read-only string (bytes). Textual description of last error when running or loading model (if any). */
+        LastError = 0x185,
     }
 
     function packArray(arr: number[], fmt: NumberFormat) {
@@ -86,67 +59,6 @@ namespace jacdac {
 
 
     const arenaSizeSettingsKey = "#jd-tflite-arenaSize"
-    const inputsSettingsKey = "#jd-tflite-inputs"
-
-    function numberFmt(stype: TFLiteSampleType) {
-        switch (stype) {
-            case TFLiteSampleType.U8: return NumberFormat.UInt8LE
-            case TFLiteSampleType.I8: return NumberFormat.Int8LE
-            case TFLiteSampleType.U16: return NumberFormat.UInt16LE
-            case TFLiteSampleType.I16: return NumberFormat.Int16LE
-            case TFLiteSampleType.U32: return NumberFormat.UInt32LE
-            case TFLiteSampleType.I32: return NumberFormat.Int32LE
-        }
-    }
-
-    class Collector extends Client {
-        private requiredServiceNum: number
-        lastSample: Buffer
-        private parent: TFLiteHost
-        private numElts: number
-        private sampleType: TFLiteSampleType
-        private sampleMult: number
-
-        handlePacket(packet: JDPacket) {
-            if (packet.service_command == (CMD_GET_REG | REG_READING)) {
-                this.parent._newData(packet.timestamp, false)
-                const arr = packet.data.toArray(numberFmt(this.sampleType))
-                for (let i = 0; i < arr.length; ++i)
-                    this.lastSample.setNumber(NumberFormat.Float32LE, i << 2, arr[i] * this.sampleMult)
-                this.parent._newData(packet.timestamp, true)
-            }
-        }
-
-        _attach(dev: Device, serviceNum: number) {
-            if (this.requiredServiceNum && serviceNum != this.requiredServiceNum)
-                return false
-            return super._attach(dev, serviceNum)
-        }
-
-        constructor(parent: TFLiteHost, config: Buffer) {
-            const [serviceClass, serviceNum, sampleSize, sampleType, sampleShift] = config.unpack("IBBBb", 8)
-            const devId = config.getNumber(NumberFormat.Int32LE, 0) == 0 ? null : config.slice(0, 8).toHex()
-            super("tfcoll", serviceClass, devId)
-            this.requiredServiceNum = serviceNum
-            this.sampleType = sampleType
-
-            this.sampleMult = 1
-            let sh = sampleShift
-            while (sh > 0) {
-                this.sampleMult /= 2
-                sh--
-            }
-            while (sh < 0) {
-                this.sampleMult *= 2
-                sh++
-            }
-
-            this.numElts = Math.idiv(sampleSize, Buffer.sizeOfNumberFormat(numberFmt(this.sampleType)))
-            this.lastSample = Buffer.create(this.numElts * 4)
-
-            this.parent = parent
-        }
-    }
 
 
     export class TFLiteHost extends Host {
@@ -154,18 +66,21 @@ namespace jacdac {
         private execTime = 0
         private outputs = Buffer.create(0)
         private lastError: string
-        private collectors: Collector[]
-        private lastSample: number
-        private samplingInterval: number
-        private samplesInWindow: number
-        private sampleSize: number
-        private streamSamples: number
-        private samplesBuffer: Buffer
-        private numSamples: number
         private lastRunNumSamples: number
 
-        constructor() {
+        constructor(private agg: SensorAggregatorHost) {
             super("tflite", jd_class.TFLITE);
+            agg.newDataCallback = () => {
+                if (this.autoInvokeSamples && this.lastRunNumSamples >= 0 &&
+                    this.numSamples - this.lastRunNumSamples >= this.autoInvokeSamples) {
+                    this.lastRunNumSamples = -1
+                    control.runInBackground(() => this.runModel())
+                }
+            }
+        }
+
+        get numSamples() {
+            return this.agg.numSamples
         }
 
         get modelBuffer() {
@@ -182,30 +97,12 @@ namespace jacdac {
             else return 0
         }
 
-        get inputSettings() {
-            return settings.readBuffer(inputsSettingsKey)
-        }
-
-        private pushData() {
-            this.samplesBuffer.shift(this.sampleSize)
-            let off = this.samplesBuffer.length - this.sampleSize
-            for (const coll of this.collectors) {
-                this.samplesBuffer.write(off, coll.lastSample)
-                off += coll.lastSample.length
-            }
-            this.numSamples++
-            if (this.streamSamples > 0) {
-                this.streamSamples--
-                this.sendLastSample()
-            }
-        }
-
         private runModel() {
             if (this.lastError) return
             const numSamples = this.numSamples
             const t0 = control.micros()
             try {
-                const res = tf.invokeModelF([this.samplesBuffer])
+                const res = tf.invokeModelF([this.agg.samplesBuffer])
                 this.outputs = packArray(res[0], NumberFormat.Float32LE)
             } catch (e) {
                 if (typeof e == "string")
@@ -217,33 +114,10 @@ namespace jacdac {
             this.sendReport(JDPacket.from(CMD_GET_REG | TFLiteReg.Outputs, this.outputs))
         }
 
-        _newData(timestamp: number, isPost: boolean) {
-            if (!this.lastSample)
-                this.lastSample = timestamp
-            const d = timestamp - this.lastSample
-            let numSamples = Math.idiv(d + (d >> 1), this.samplingInterval)
-            if (!numSamples)
-                return
-            if (isPost) {
-                this.lastSample = timestamp
-                this.pushData()
-                if (this.autoInvokeSamples && this.lastRunNumSamples >= 0 &&
-                    this.numSamples - this.lastRunNumSamples >= this.autoInvokeSamples) {
-                    this.lastRunNumSamples = -1
-                    control.runInBackground(() => this.runModel())
-                }
-            } else {
-                numSamples--
-                if (numSamples > 5) numSamples = 5
-                while (numSamples-- > 0)
-                    this.pushData()
-            }
-        }
-
         start() {
             super.start()
+            this.agg.start()
             this.loadModel()
-            this.configureInputs()
         }
 
         private eraseModel() {
@@ -268,46 +142,6 @@ namespace jacdac {
                     this.lastError = e
                 control.dmesgValue(e)
             }
-        }
-
-        private configureInputs() {
-            const config = this.inputSettings
-            if (!config)
-                return
-            /*
-            rw inputs @ 0x80 {
-                sampling_interval: u16 ms
-                samples_in_window: u16
-                reserved: u32
-            repeats:
-                device_id: u64
-                service_class: u32
-                service_num: u8
-                sample_size: u8 bytes
-                sample_type: SampleType
-                sample_shift: i8
-            }
-            */
-
-            [this.samplingInterval, this.samplesInWindow] = config.unpack("HH")
-            const entrySize = 16
-            let off = 8
-            for (const coll of this.collectors || [])
-                coll.destroy()
-            this.collectors = []
-            let frameSz = 0
-            while (off < config.length) {
-                const coll = new Collector(this, config.slice(off, entrySize))
-                coll.setRegInt(REG_STREAMING_INTERVAL, this.samplingInterval)
-                coll.setRegInt(REG_IS_STREAMING, 255)
-                this.collectors.push(coll)
-                frameSz += coll.lastSample.length
-                off += entrySize
-            }
-            this.sampleSize = frameSz
-            this.samplesBuffer = Buffer.create(this.samplesInWindow * frameSz)
-            this.numSamples = 0
-            this.lastRunNumSamples = 0
         }
 
         private readModel(packet: JDPacket) {
@@ -348,19 +182,11 @@ namespace jacdac {
             this.loadModel()
         }
 
-        private sendLastSample() {
-            const buf = this.samplesBuffer.slice(this.samplesBuffer.length - this.sampleSize, this.sampleSize)
-            this.sendReport(JDPacket.from(TFLiteReg.CurrentSample | CMD_GET_REG, buf))
-        }
-
         handlePacket(packet: JDPacket) {
             this.handleRegInt(packet, TFLiteReg.AllocatedArenaSize, tf.arenaBytes())
             this.handleRegInt(packet, TFLiteReg.LastRunTime, this.execTime)
             this.handleRegInt(packet, TFLiteReg.ModelSize, this.modelSize)
-            this.handleRegInt(packet, TFLiteReg.NumSamples, this.numSamples)
-            this.handleRegInt(packet, TFLiteReg.SampleSize, this.sampleSize)
             this.handleRegBuffer(packet, TFLiteReg.Outputs, this.outputs)
-            this.streamSamples = this.handleRegInt(packet, TFLiteReg.StreamSamples, this.streamSamples)
             this.autoInvokeSamples = this.handleRegInt(packet, TFLiteReg.AutoInvokeEvery, this.autoInvokeSamples)
 
             let arr: number[]
@@ -368,23 +194,11 @@ namespace jacdac {
                 case TFLiteCmd.SetModel:
                     control.runInBackground(() => this.readModel(packet))
                     break
-                case TFLiteReg.Inputs | CMD_GET_REG:
-                    this.sendReport(JDPacket.from(packet.service_command, this.inputSettings))
-                    break
-                case TFLiteReg.Inputs | CMD_SET_REG:
-                    if (this.inputSettings && packet.data.equals(this.inputSettings))
-                        return // already done
-                    settings.writeBuffer(inputsSettingsKey, packet.data)
-                    this.configureInputs()
-                    break
                 case TFLiteReg.OutputShape | CMD_GET_REG:
                     arr = tf.outputShape(0)
                 case TFLiteReg.InputShape | CMD_GET_REG:
                     arr = arr || tf.inputShape(0)
                     this.sendReport(JDPacket.from(packet.service_command, packArray(arr, NumberFormat.UInt16LE)))
-                    break;
-                case TFLiteReg.CurrentSample | CMD_GET_REG:
-                    this.sendLastSample()
                     break;
                 case TFLiteReg.LastError | CMD_GET_REG:
                     this.sendReport(JDPacket.from(packet.service_command, Buffer.fromUTF8(this.lastError || "")))
@@ -396,5 +210,5 @@ namespace jacdac {
     }
 
     //% whenUsed
-    export const tfliteHost = new TFLiteHost()
+    export const tfliteHost = new TFLiteHost(sensorAggregatorHost)
 }
